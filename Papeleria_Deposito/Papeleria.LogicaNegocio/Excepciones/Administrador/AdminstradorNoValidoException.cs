3c8cbecf5e64331f07547f259304fb967ebcf99e
Gianfranco Bonanni <110034721+GianfrancoB23@users.noreply.github.com>
2024-06-17 10:23:56 -0300

    update

--- Papeleria_Deposito/Papeleria.LogicaNegocio/Excepciones/Administrador/AdminstradorNoValidoException.cs
+++ Papeleria_Deposito/Papeleria.LogicaNegocio/Excepciones/Administrador/AdminstradorNoValidoException.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Papeleria.LogicaNegocio.Excepciones.Administrador
 {
-    internal class AdminstradorNoValidoException
+    public class AdminstradorNoValidoException : Exception
     {
+        public AdminstradorNoValidoException()
+        {
+        }
+
+        public AdminstradorNoValidoException(string? message) : base(message)
+        {
+        }
+
+        public AdminstradorNoValidoException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected AdminstradorNoValidoException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
--- Papeleria_Deposito/Papeleria.LogicaNegocio/Excepciones/EncargadoDeposito/EncargadoNoValidoException.cs
+++ Papeleria_Deposito/Papeleria.LogicaNegocio/Excepciones/EncargadoDeposito/EncargadoNoValidoException.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Papeleria.LogicaNegocio.Excepciones.EncargadoDeposito
 {
-    internal class EncargadoNoValidoException
+    public class EncargadoNoValidoException : Exception
     {
+        public EncargadoNoValidoException()
+        {
+        }
+
+        public EncargadoNoValidoException(string? message) : base(message)
+        {
+        }
+
+        public EncargadoNoValidoException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected EncargadoNoValidoException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
