d910252225b05d9c16d2aa9ffcb2455fa470d230
Gabriel Moreno <ganimomu2001@gmail.com>
2024-06-19 11:39:58 -0300

    Movimientos

--- Papeleria_Deposito/Papeleria.LogicaAplicacion/ImplementacionCasosUso/Movimiento/UpdateMovimiento.cs
+++ Papeleria_Deposito/Papeleria.LogicaAplicacion/ImplementacionCasosUso/Movimiento/UpdateMovimiento.cs
@@ -1,5 +1,10 @@
-﻿using Papeleria.LogicaAplicacion.Interaces;
+﻿using Papeleria.LogicaAplicacion.DataTransferObjects.Dtos.MovimientoStock;
+using Papeleria.LogicaAplicacion.DataTransferObjects.MapeosDatos;
+using Papeleria.LogicaAplicacion.Interaces;
+using Papeleria.LogicaAplicacion.InterfacesCasosUso.MovimientoStock;
 using Papeleria.LogicaNegocio.Entidades;
+using Papeleria.LogicaNegocio.Excepciones.Articulo;
+using Papeleria.LogicaNegocio.InterfacesRepositorio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +13,26 @@ using System.Threading.Tasks;
 
 namespace Papeleria.LogicaAplicacion.ImplementacionCasosUso.Movimiento
 {
-    public class UpdateMovimiento : IUpdate<MovimientoStock>
+    public class UpdateMovimiento : IUpdateMovimiento
     {
-        public void Update(int id, MovimientoStock obj)
+        private IRepositorioMovimientoStock _repo;
+        private IRepositorioArticulo _repoArt;
+        private IRepositorioUsuario _repoUsr;
+        public UpdateMovimiento(IRepositorioMovimientoStock repo, IRepositorioArticulo repoArt, IRepositorioUsuario repoUsr)
         {
-            throw new NotImplementedException();
+            _repo = repo;
+            _repoArt = repoArt;
+            _repoUsr = repoUsr;
+        }
+        public void Update(int id, MovimientoStockDTO obj)
+        {
+            if (obj == null) { throw new Exception("Articulo modificado no puede ser nulo"); } // TODO exception handler
+            try
+            {
+                var mov = MovimientoStockMappers.FromDTOUpdate(obj, _repoArt, _repoUsr);
+                _repo.Update(id, mov);
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
         }
     }
 }
--- Papeleria_Deposito/Papeleria.WebApi/Controllers/MovimientosController.cs
+++ Papeleria_Deposito/Papeleria.WebApi/Controllers/MovimientosController.cs
@@ -1,4 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Empresa.LogicaDeNegocio.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Papeleria.AccesoDatos.EF;
+using Papeleria.LogicaAplicacion.DataTransferObjects.Dtos.Articulos;
+using Papeleria.LogicaAplicacion.DataTransferObjects.Dtos.MovimientoStock;
+using Papeleria.LogicaAplicacion.ImplementacionCasosUso.Movimiento;
+using Papeleria.LogicaAplicacion.InterfacesCasosUso.Articulos;
+using Papeleria.LogicaAplicacion.InterfacesCasosUso.Movimientos;
+using Papeleria.LogicaAplicacion.InterfacesCasosUso.MovimientoStock;
+using Papeleria.LogicaNegocio.Excepciones.Articulo;
+using Papeleria.LogicaNegocio.InterfacesRepositorio;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +18,132 @@ namespace Papeleria.WebApi.Controllers
     [ApiController]
     public class MovimientosController : ControllerBase
     {
+        private PapeleriaContext _context;
+        private IRepositorioArticulo _repoArt;
+        private IRepositorioUsuario _repoUsr;
+        private IRepositorioMovimientoStock _repo;
+        private IGetAllArticulos _cuGetArticulos;
+        private IGetArticulo _cuGetArticulo;
+        private IAltaArticulo _cuAltaArticulo;
+        private IBorrarArticulo _cuBorrarArticulo;
+        private IUpdateArticulo _cuModificarArticulo;
+        private IGetMovimiento _cuGetMovimiento;
+        private IAltaMovimiento _cuAltaMovimiento;
+        private IBorrarMovimiento _cuBorrarMovimiento;
+        private IUpdateMovimiento _cuUpdateMovimiento;
+
+        public MovimientosController(IRepositorioArticulo repoArt,IRepositorioUsuario repoUsr, IRepositorioMovimientoStock repo, IAltaArticulo cuAltaArticulo, IGetAllArticulos cuGetArticulos,
+            IGetArticulo cuGetArticulo, IBorrarArticulo cuBorrarArticulo, IUpdateArticulo cuModificarArticulo, IAltaMovimiento altaMovimiento, IBorrarMovimiento borrarMovimiento, IUpdateMovimiento updateMovimiento, IGetMovimiento getMovimiento)
+        {
+            _repoArt = repoArt;
+            _repoUsr = repoUsr;
+            _repo = repo;
+            _cuGetArticulos = cuGetArticulos;
+            _cuGetArticulo = cuGetArticulo;
+            _cuAltaArticulo = cuAltaArticulo;
+            _cuBorrarArticulo = cuBorrarArticulo;
+            _cuModificarArticulo = cuModificarArticulo; 
+            _cuAltaMovimiento = altaMovimiento;
+            _cuBorrarMovimiento = borrarMovimiento;
+            _cuUpdateMovimiento = updateMovimiento;
+            _cuGetMovimiento = getMovimiento;
+        }
         // GET: api/<Movimientos>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<IEnumerable<MovimientoStockDTO>> Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                var movimientosDto = _cuGetMovimiento.GetAll();
+                var ordenada = movimientosDto.OrderBy(movimientos => movimientos.NombreArticulo);
+                return Ok(ordenada);
+            }
+            catch (ArticuloNoValidoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         // GET api/<Movimientos>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ActionResult<MovimientoStockDTO> Get(int id)
         {
-            return "value";
+            try
+            {
+                var movimientosDto = _cuGetMovimiento.Get(id);
+                return Ok(movimientosDto);
+            }
+            catch (ArticuloNoValidoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         // POST api/<Movimientos>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<MovimientoStockDTO> Post([FromBody] MovimientoStockDTO mov)
         {
+            try
+            {
+                _cuAltaMovimiento.Crear(mov);
+                return CreatedAtRoute("Get", new { id = mov.ID }, mov);
+            }
+
+            catch (ArticuloNoValidoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
         }
 
         // PUT api/<Movimientos>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult<MovimientoStockDTO> Put(int id, [FromBody] MovimientoStockDTO mov)
         {
+            try
+            {
+                _cuUpdateMovimiento.Update(id, mov);
+                return Ok(mov);
+            }
+            catch (ArticuloNoValidoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         // DELETE api/<Movimientos>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult<MovimientoStockDTO> Delete(int id)
         {
+            try
+            {
+                _cuBorrarMovimiento.Remove(id);
+                return Ok();
+            }
+            catch (ArticuloNoValidoException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
     }
 }
