6a1a24e3d85931e5e6a29c84a66d2b8f95893321
Gabriel Moreno <ganimomu2001@gmail.com>
2024-06-20 01:00:56 -0300

    push

---
+++ Papeleria_Deposito/Papeleria.AccesoDatos/Migrations/20240620035748_discriminator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Papeleria.AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class discriminator : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Discriminator",
+                table: "Usuarios",
+                newName: "Tipo");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Tipo",
+                table: "Usuarios",
+                newName: "Discriminator");
+        }
+    }
+}
