df8254f012a0eb931d3e16fc702d4f75ad13cb18
Gianfranco Bonanni <110034721+GianfrancoB23@users.noreply.github.com>
2024-06-20 10:07:43 -0300

    poblar tabas

---
+++ PoblarTablas/Articulos.sql
@@ -0,0 +1,17 @@
+﻿SET IDENTITY_INSERT [dbo].[Articulos] ON 
+
+INSERT [dbo].[Articulos] ([Id], [PrecioVP], [CodigoProveedor_codigo], [Descripcion_Descripcion], [NombreArticulo_Nombre]) VALUES (1, 500, 1234567891231, N'Pack Hojas de tamaño A4', N'Hoja A4')
+INSERT [dbo].[Articulos] ([Id], [PrecioVP], [CodigoProveedor_codigo], [Descripcion_Descripcion], [NombreArticulo_Nombre]) VALUES (2, 50, 9876543210981, N'Tijeras escolares', N'Tijeras')
+INSERT [dbo].[Articulos] ([Id], [PrecioVP], [CodigoProveedor_codigo], [Descripcion_Descripcion], [NombreArticulo_Nombre]) VALUES (3, 90, 4561237894561, N'Cuadernos de 100 hojas', N'Cuaderno')
+INSERT [dbo].[Articulos] ([Id], [PrecioVP], [CodigoProveedor_codigo], [Descripcion_Descripcion], [NombreArticulo_Nombre]) VALUES (4, 30, 7891234567891, N'Lápices HB', N'Lápiz HB')
+INSERT [dbo].[Articulos] ([Id], [PrecioVP], [CodigoProveedor_codigo], [Descripcion_Descripcion], [NombreArticulo_Nombre]) VALUES (5, 1000, 3216549871231, N'Mochilas escolares', N'Mochila')
+INSERT [dbo].[Articulos] ([Id], [PrecioVP], [CodigoProveedor_codigo], [Descripcion_Descripcion], [NombreArticulo_Nombre]) VALUES (6, 25, 6549873214561, N'Gomas de borrar', N'Goma de borrar')
+INSERT [dbo].[Articulos] ([Id], [PrecioVP], [CodigoProveedor_codigo], [Descripcion_Descripcion], [NombreArticulo_Nombre]) VALUES (7, 200, 7894561237819, N'Calculadoras científicas', N'Calculadora')
+INSERT [dbo].[Articulos] ([Id], [PrecioVP], [CodigoProveedor_codigo], [Descripcion_Descripcion], [NombreArticulo_Nombre]) VALUES (8, 23, 1237896543121, N'Cartulinas de colores', N'Cartulina')
+INSERT [dbo].[Articulos] ([Id], [PrecioVP], [CodigoProveedor_codigo], [Descripcion_Descripcion], [NombreArticulo_Nombre]) VALUES (9, 15, 4567891231987, N'Pegamento en barra', N'Pegamento')
+INSERT [dbo].[Articulos] ([Id], [PrecioVP], [CodigoProveedor_codigo], [Descripcion_Descripcion], [NombreArticulo_Nombre]) VALUES (10, 30, 1122334451566, N'Bolígrafos azules', N'Bolígrafo')
+INSERT [dbo].[Articulos] ([Id], [PrecioVP], [CodigoProveedor_codigo], [Descripcion_Descripcion], [NombreArticulo_Nombre]) VALUES (11, 28, 2233445516677, N'Resaltadores', N'Resaltador')
+INSERT [dbo].[Articulos] ([Id], [PrecioVP], [CodigoProveedor_codigo], [Descripcion_Descripcion], [NombreArticulo_Nombre]) VALUES (12, 60, 3344556617788, N'Libretas de notas', N'Libreta')
+INSERT [dbo].[Articulos] ([Id], [PrecioVP], [CodigoProveedor_codigo], [Descripcion_Descripcion], [NombreArticulo_Nombre]) VALUES (13, 150, 4455667718899, N'Grampas para engrapadora', N'Grampas')
+INSERT [dbo].[Articulos] ([Id], [PrecioVP], [CodigoProveedor_codigo], [Descripcion_Descripcion], [NombreArticulo_Nombre]) VALUES (14, 17, 5566778891900, N'Notas adhesivas', N'Post-it')
+SET IDENTITY_INSERT [dbo].[Articulos] OFF
---
+++ PoblarTablas/TipoMovimientos.sql
@@ -0,0 +1,11 @@
+﻿SET IDENTITY_INSERT [dbo].[TiposMovimientos] ON;
+INSERT [dbo].[TiposMovimientos] ([ID], [Nombre])
+VALUES
+(1, 'Venta'),
+(2, 'Devolucion'),
+(3, 'Entrada'),
+(4, 'Roptura'),
+(5, 'Reacondicionamiento'),
+(6, 'Service'),
+(7, 'Otro');
+SET IDENTITY_INSERT [dbo].[TiposMovimientos] OFF;
