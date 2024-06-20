96ce8a63956d0059333a851e86e194b6ef90999a
Gianfranco Bonanni <110034721+GianfrancoB23@users.noreply.github.com>
2024-06-20 11:29:13 -0300

    Consultas MVC

---
+++ Papeleria.MVC/Controllers/ConsultasController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Papeleria.MVC.Controllers
+{
+    public class ConsultasController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _url = "https://localhost:7148/api/";
+        private readonly JsonSerializerOptions _jsonOptions
+            = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        // GET: MovimientoStockController
+        public ConsultasController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(_url);
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        // GET: ConsultasController
+        public ActionResult Index()
+        {
+            ViewBag.Usuarios = _httpClient.GetAsync("Usuarios").Result;
+            return View();
+        }
+
+        // GET: ConsultasController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ConsultasController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ConsultasController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ConsultasController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ConsultasController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ConsultasController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ConsultasController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Papeleria.MVC/Models/UsuarioModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Papeleria.MVC.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public int Admin { get; set; }
+        public string Email { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string Contrasenia { get; set; }
+    }
+}
